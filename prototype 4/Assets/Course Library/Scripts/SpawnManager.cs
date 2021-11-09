f8ca689fdc64ce27be5b73e8ec0327d7d7a9e17a
Sam Stallings <10915865@my.uvu.edu>
2021-11-09 14:56:49 -0700

    finished proto 4

--- prototype 4/Assets/Course Library/Scripts/SpawnManager.cs
+++ prototype 4/Assets/Course Library/Scripts/SpawnManager.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    public GameObject powerupPrefab;
     public GameObject enemyPrefab;
     private float spawnRange = 9;
     public int enemyCount;
+    public int waveNumber = 1;
 
     // Start is called before the first frame update
     void Start()
     {
-        SpawnEnemyWave(3);
+        SpawnEnemyWave(waveNumber);
+        Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
     }
 
     private Vector3 GenerateSpawnPosition()
@@ -25,7 +28,14 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.y < -10) { Destroy(gameObject); }
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+
+        if(enemyCount == 0)
+        {
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
+            Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+        }
     }
 
     
---
+++ prototype 4/Assets/Course Library/Scripts/StartRotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartRotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 rotateSpeed = new Vector3(0.0f, 0.5f, 0.0f);
+        transform.Rotate(rotateSpeed);
+    }
+}
